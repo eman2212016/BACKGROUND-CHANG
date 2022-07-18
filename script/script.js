ce7437190d4d9ad68edf4d4d603f0b2af9d75e24
eman2212016 <emmyhanafy83@gmail.com>
2022-07-19 01:07:52 +0200

    Background chang

---
+++ script/script.js
@@ -0,0 +1,11 @@
+let img=document.querySelector('.image');
+let container =document.querySelector('.container-fluid');
+
+
+function mobile(mob){
+    img.src=mob;
+}
+
+function  Colors(color){
+    container.style.background = color ;
+}
